c726fd40cf6d4f1d61df535d0c9aed98339a9e16
Vaishnav <vshnav51@gmail.com>
2018-11-28 16:39:51 +0530

    Added conflict

--- Patterns/src/Pattern2.java
+++ Patterns/src/Pattern2.java
@@ -19,7 +19,7 @@ public class Pattern2 {
 			}
 			System.out.println("Vaishnav's Changes");
 			System.out.println("Sharath's Changes");
-			
+			System.out.println("This will be a conflict");
 		}
 		for(int j=n-1;j>=1;j--)
 		{
