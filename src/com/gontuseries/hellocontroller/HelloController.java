7c4ac6cf415079ac48cffda5115e0de2d1191926
sharat550 <sharatchittapur@gmail.com>
2019-02-19 14:01:47 +0530

    adding files

---
+++ src/com/gontuseries/hellocontroller/HelloController.java
@@ -0,0 +1,19 @@
+package com.gontuseries.hellocontroller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+public class HelloController extends AbstractController{
+
+	
+	protected ModelAndView handleRequestInternal(HttpServletRequest req,HttpServletResponse res) throws Exception
+	{
+		ModelAndView model = new ModelAndView("HelloPage");
+		model.addObject("welcomemessage", "Hi user welcome to the spring app");
+		
+		return model;
+	}
+}
